9a14f5955e7f2b48cbddb220851cf30a2473f72c
Paul Anuschek <paul.anuschek@novatec-gmbh.de>
2024-03-18 16:15:44 +0000

    Aufgabe_1 Lösung

--- todo-backend/src/main/java/de/pan/todobackend/TodoBackendApplication.java
+++ todo-backend/src/main/java/de/pan/todobackend/TodoBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodoBackendApplication.class, args);
 	}
-
 }
---
+++ todo-backend/src/main/java/de/pan/todobackend/services/HelloService.java
@@ -0,0 +1,10 @@
+package de.pan.todobackend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String Greeting() {
+        return "Hello World!";
+    }
+}
