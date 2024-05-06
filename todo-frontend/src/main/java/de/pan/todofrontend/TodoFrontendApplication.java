c3d352d6750866839085d2f86e5fe51a296ab483
Paul Anuschek <paul.anuschek@novatec-gmbh.de>
2024-05-06 13:19:51 +0000

    added frontend

---
+++ todo-frontend/src/main/java/de/pan/todofrontend/TodoFrontendApplication.java
@@ -0,0 +1,13 @@
+package de.pan.todofrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoFrontendApplication.class, args);
+	}
+
+}
